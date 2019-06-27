b05ec318f7e9d66b48f7e505ee4a932a209049e6
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-27 17:30:12 +0100

    Added logic for checking if input was a value of the name of a boolean

--- js/app.js
+++ js/app.js
@@ -3,10 +3,18 @@
 window.onload = function(event) {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
 
+	function check(value) {
+		if (value === "true" || value === "false") {
+			return "Boolean";
+		}
+
+		return "Not Boolean";
+	}
+
 	let form = document.getElementById("form");
 	form.addEventListener("submit", function(event) {
 		event.preventDefault();
 
-		
+		check(this.children[0].value);
 	});
 };
